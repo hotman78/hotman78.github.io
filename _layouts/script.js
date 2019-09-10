ed54626b0edb4b0b3ba085f154dc91d4185dd60b
hotman78 <hotmanww@gmail.com>
2019-09-10 19:51:01 +0900

    add

--- _layouts/script.js
+++ _layouts/script.js
@@ -1,3 +1,11 @@
 hljs.initHighlightingOnLoad();
-$('code').load("./code/segment-1.cpp");
-    
+function disp(path,index){
+    $.get(path, function(data){
+        $("#code"+index).text(data);
+        $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+    });
+}
+function _close(index){
+    $("#code"+index).empty();
+    //$('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+}
